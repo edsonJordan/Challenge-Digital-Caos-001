3c3a1346fe0591b6bd8db38d6b0cd2fc4881ec43
edsonJordan <edson_4555@hotmail.com>
2020-08-23 13:54:45 -0500

    javascript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ bShared.addEventListener('click', (e)=>{
     const shared = document.getElementById("shared");    
     shared.classList.toggle("none")
     bShared.classList.toggle("active");        
-    if(screen.availWidth >1300 ){
+    if(screen.availWidth >1000 ){
         shared.style.left=JSON.stringify((bShared.offsetLeft)-((bShared.clientWidth)*3.5))+ "px";
         shared.style.top = JSON.stringify((bShared.offsetTop)-((bShared.clientHeight)*3.5)) + "px";          
     }    
