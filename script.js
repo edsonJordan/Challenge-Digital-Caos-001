ee704395cb960f62d02411d94eeab456271e04f0
edsonJordan <edson_4555@hotmail.com>
2020-08-23 02:19:11 -0500

    article end

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ bShared.addEventListener('click', (e)=>{
     const profile = document.getElementById("profile");
     const shared = document.getElementById("shared");    
     shared.classList.toggle("none")
-    bShared.classList.toggle("active");    
+    bShared.classList.toggle("active");        
     if(screen.availWidth >1300 ){
-        console.log(JSON.stringify((e.pageX)-25));                
-    }
-    
+        shared.style.left=JSON.stringify((bShared.offsetLeft)-((bShared.clientWidth)*3.5) )+ "px";
+        shared.style.top = JSON.stringify((bShared.offsetTop)-((bShared.clientHeight)*3.5) ) + "px";          
+    }    
 })
